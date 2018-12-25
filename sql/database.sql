cf1eb067e59f74bd1865bc315f2a70084565e466
田村侑大 <yudai@YudaiMacBookAir.local>
2018-12-25 19:03:43 +0900

    テーブル定義をした

---
+++ sql/database.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS `singer` (
+  `id`   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `song` (
+  `id`        INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title`     VARCHAR(50),
+  `lyric`     TEXT DEFAULT NULL,
+  `singer_id` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE(`title`, `lyric`(50)),
+  CONSTRAINT `fk_singer` FOREIGN KEY (`singer_id`) REFERENCES `singer` (`id`)
+  ON DELETE NO ACTION ON UPDATE CASCADE
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id`        INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`      VARCHAR(50),
+  `password`  VARCHAR(50),
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `user_song` (
+  `user_id` INT(10) UNSIGNED NOT NULL,
+  `song_id` INT(10) UNSIGNED NOT NULL,
+  UNIQUE(`user_id`,`song_id`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_song` FOREIGN KEY (`song_id`) REFERENCES `song` (`id`)
+  ON DELETE NO ACTION ON UPDATE CASCADE
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
