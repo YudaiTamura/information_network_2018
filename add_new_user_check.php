15d3ac0fd2d0c25ec1a5dc6a1a3389e07a1b630b
田村侑大 <yudai@YudaiMacBookAir.local>
2018-12-25 22:06:34 +0900

    ログイン・新規ユーザ登録まわりの機能を実装

---
+++ add_new_user_check.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>新規ユーザ登録 | Lyrics Parser | 情報ネットワーク特論2018</title>
+    <meta name="description" content="歌詞をパースしてくれるWEBアプリケーション。">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="image/favicon.ico">
+</head>
+<body>
+<header class="site-header">
+    <h1 class="site-header__title">Lyrics Parser</h1>
+</header>
+
+<main id="top" class="site-main" role="main">
+    <article class="site-main__content">
+        <section class="add-new-user-check">
+            <div class="add-new-user-check__wrapper">
+                <?php
+                $userName = htmlspecialchars($_POST['username']);
+                $password = htmlspecialchars($_POST['password']);
+                $passwordAgain = htmlspecialchars($_POST['password-again']);
+
+                if ($userName) { ?>
+                    <p class="add-new-user-check__wrapper__content">ユーザ名： <?= $userName; ?></p>
+                <?php } else { ?>
+                    <p class="add-new-user-check__wrapper__content font-red">ユーザ名が入力されていません</p>
+                <?php }
+
+                $passwordConditionFulfilled = preg_match('/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,50}+\z/', $password);
+
+                if (empty($password)) { ?>
+                    <p class="add-new-user-check__wrapper__content font-red">パスワードが入力されていません</p>
+                <?php } else if ($password != $passwordAgain) { ?>
+                    <p class="add-new-user-check__wrapper__content font-red">パスワードが一致しません</p>
+                <?php } else { // パスワードが入力されていて、かつ再度入力したものも一致しているとき
+                    if (!$passwordConditionFulfilled) { ?>
+                        <p class="add-new-user-check__wrapper__content font-red">
+                            小文字、大文字、数字をそれぞれ1種類以上含む8文字以上のパスワードを設定してください
+                        </p>
+                    <?php } else {
+                        $display_password = '';
+                        for ($i = 1; $i <= mb_strlen($password); $i++) {
+                            $display_password = $display_password . '･';
+                        } ?>
+                        <p class="add-new-user-check__wrapper__content">
+                            パスワード：<?= $display_password; ?>
+                        </p>
+                    <?php }
+                } ?>
+            </div>
+
+            <form class="add-new-user-check__form" method="post" action="add_new_user_done.php">
+                <button type="button" onclick="history.back()">戻る</button>
+                <?php if ($userName && $password && $password == $passwordAgain && $passwordConditionFulfilled) { ?>
+                    <input name="username" type="hidden" value="<?= $userName; ?>">
+                    <input name="password" type="hidden" value="<?= password_hash($password, PASSWORD_BCRYPT); ?>">
+                    <button type="submit">追加</button>
+                <?php } ?>
+            </form>
+        </section>
+    </article>
+</main>
+
+<footer class="site-footer">
+    <p class="site-footer__content">© <?= date('Y'); ?> Yudai TAMURA</p>
+</footer>
+</body>
+</html>
---
+++ add_new_user_done.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>新規ユーザ登録 | Lyrics Parser | 情報ネットワーク特論2018</title>
+    <meta name="description" content="歌詞をパースしてくれるWEBアプリケーション。">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="image/favicon.ico">
+</head>
+<body>
+<header class="site-header">
+    <h1 class="site-header__title">Lyrics Parser</h1>
+</header>
+
+<main id="top" class="site-main" role="main">
+    <article class="site-main__content">
+        <?php
+        $pdo = new PDO(
+            'mysql:dbname=lyrics_parser;host=localhost;',
+            'root',
+            'root',
+            [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4']
+        );
+
+        $userName = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password']);
+
+        try {
+            $statement = $pdo->prepare('INSERT INTO `user` (`name`, `password`) VALUES (?, ?)');
+            $statement->execute([$userName, $password]);
+        } catch (PDOException $e) {
+            header('Content-Type: text/plain; charset=UTF-8', true, 500);
+            exit($e->getMessage());
+        }
+
+        $pdo = null; ?>
+
+        <section class="add-new-user-done">
+            <p class="add-new-user-done__text">
+                新しいスタッフ <?= $userName; ?> さんを追加しました。
+            </p>
+            <a class="add-new-user-done__link" href="lyrics_list.php">歌詞リストへ</a>
+        </section>
+
+    </article>
+</main>
+
+<footer class="site-footer">
+    <p class="site-footer__content">© <?= date('Y'); ?> Yudai TAMURA</p>
+</footer>
+</body>
+</html>
